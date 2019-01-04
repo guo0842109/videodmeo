b8ecb1de5b5af91c0a6a7fb5daa3f2eb167c9b7a
guohaibing <guohaibing@yto.net.cn>
2019-01-04 16:45:16 +0800

    新增tips

--- app/src/main/java/com/eric/videodemo/ui/DrawableSurfaceViewActivity.java
+++ app/src/main/java/com/eric/videodemo/ui/DrawableSurfaceViewActivity.java
@@ -10,6 +10,8 @@ import com.eric.videodemo.R;
  * Author: Created by guohaibing
  * Date: on 2019/1/4  16 :31
  * Email: guohaibing@yto.net.cn
+ * 关于 SurfaceView，有一个知识点是，渲染到 Surface 上的内容将会由 SurfaceFlinger（而非应用）进行合成，
+ * 因此获得的 Surface 可以由单独的线程或单独的进程进行渲染，并与应用界面执行的任何渲染隔离开。
  */
 public class DrawableSurfaceViewActivity extends Activity{
 
